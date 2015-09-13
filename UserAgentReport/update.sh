1f1a7a8250b3e748f318d0a80699c7ce8a9a2361
Joel Verhagen <joel.verhagen@gmail.com>
2015-09-12 19:28:03 -0700

    Update bash script

--- UserAgentReport/update.sh
+++ UserAgentReport/update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TOOL=$DIR/Knapcode.UserAgentReport.exe
+LATEST=$DIR/user-agents-latest
 
-$DIR/Knapcode.UserAgentReport.exe -populate -query > $DIR/user-agents-latest.json
-mv $DIR/user-agents-latest.json $DIR/user-agents.json
+rm -f $LATEST.sqlite3
+
+$TOOL -db $LATEST.sqlite3 -populate
+$TOOL -db $LATEST.sqlite3 -query > $LATEST.json
+
+mv $LATEST.sqlite3 $DIR/user-agents.sqlite
+mv $LATEST.json $DIR/user-agents.json
