714a7296f09f6b13a794416e4d2bb949eef00d39
Joel Verhagen <joel.verhagen@gmail.com>
2015-09-15 22:18:41 -0700

    Clean up controller

--- UserAgentReport.WebApi/Controllers/UserAgentController.cs
+++ UserAgentReport.WebApi/Controllers/UserAgentController.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Web.Http;
 using Knapcode.UserAgentReport.Reporting;
 using Knapcode.UserAgentReport.WebApi.BusinessLogic;
@@ -10,20 +8,14 @@ namespace Knapcode.UserAgentReport.WebApi.Controllers
     public class UserAgentController : ApiController
     {
         private readonly UserAgentDatabase _database;
-        private readonly UserAgentDatabaseUpdater _updater;
 
         public UserAgentController()
         {
             _database = Singletons.UserAgentDatabase;
-            _updater = Singletons.UserAgentDatabaseUpdater;
         }
 
         [HttpGet, Route("api/v1/top-user-agents")]
-        public async Task<IEnumerable<TopUserAgent>> GetTopUserAgentsAsync(
-            CancellationToken cancellationToken,
-            int limit = 50,
-            bool bots = false,
-            bool browsers = true)
+        public IEnumerable<TopUserAgent> GetTopUserAgentsAsync(int limit = 10, bool bots = false, bool browsers = true)
         {
             return _database.GetTopUserAgents(limit, bots, browsers);
         }
