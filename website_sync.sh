278d60f713414641719b5351d875af8d88ecc100
Joel Verhagen <joel.verhagen@gmail.com>
2016-09-09 07:43:01 -0700

    Remove unneeded files

--- website_sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s --data '' 'http://useragentreport.azurewebsites.net/api/v1/management/update-user-agent-database' | python -m json.tool
--- website_warmup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s 'http://useragentreport.azurewebsites.net/api/v1/top-user-agents?limit=1' | python -m json.tool
